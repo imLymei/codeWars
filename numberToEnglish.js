8506c0d572b40ed6650c04037e563de0b810470b
Lymei <lymeicontato@gmail.com>
2023-06-03 02:52:14 -0300

    feat: finalized transform number to english

--- numberToEnglish.js
+++ numberToEnglish.js
@@ -1,26 +1,114 @@
+// https://www.codewars.com/kata/5463c8db865001c1710003b2
+
 const numberToEnglish = (n) => {
-	let tempN = n.toString().split('');
+	try {
+		let tempN = n.toString().split('');
 
-	const numbersMap = [
-		'zero',
-		['one', 'ten', 'eleven'],
-		['two', 'twelve', 'twenty'],
-		['three', 'thirteen', 'thirty'],
-		['four', 'fourteen', 'fourty'],
-		['five', 'fifteen', 'fifty'],
-		['six', 'sixteen', 'sixty'],
-		['seven', 'seventeen', 'seventy'],
-		['eight', 'eighteen', 'eighty'],
-		['nine', 'nineteen', 'ninety'],
-		['hundred', 'thousand'],
-	];
+		const numbersMap = [
+			['zero', 'ten'],
+			['one', 'eleven'],
+			['two', 'twelve', 'twenty'],
+			['three', 'thirteen', 'thirty'],
+			['four', 'fourteen', 'forty'],
+			['five', 'fifteen', 'fifty'],
+			['six', 'sixteen', 'sixty'],
+			['seven', 'seventeen', 'seventy'],
+			['eight', 'eighteen', 'eighty'],
+			['nine', 'nineteen', 'ninety'],
+			['hundred', 'thousand'],
+		];
 
-	tempN.map((number, index) => {
-		if (number == '0' && tempN.length == 0) {
-			console.log('zero');
+		let response = [];
+
+		function printOne(number) {
+			response.push(numbersMap[number][0]);
 		}
-		return number;
-	});
+
+		function printTen(number, index) {
+			if (number != '0') {
+				if (number == '1') {
+					response.push(numbersMap[tempN[index + 1]][1]);
+				} else {
+					response.push(numbersMap[number][2]);
+				}
+			}
+		}
+
+		function printHundred(number) {
+			if (number != 0) {
+				response.push(numbersMap[number][0]);
+				response.push(numbersMap[10][0]);
+			}
+		}
+
+		tempN.map((number, index) => {
+			switch (tempN.length) {
+				case 1:
+					printOne(number);
+					break;
+				case 2:
+					if (index == 0) {
+						printTen(number, index);
+					} else {
+						if (number != '0' && tempN[index - 1] != '1') {
+							printOne(number);
+						}
+					}
+					break;
+				case 3:
+					if (index == 0) {
+						printHundred(number);
+					} else if (index == 1) {
+						printTen(number, index);
+					} else {
+						if (number != '0' && tempN[index - 1] != '1') {
+							printOne(number);
+						}
+					}
+					break;
+				case 4:
+					if (index == 0) {
+						response.push(numbersMap[number][0]);
+						response.push(numbersMap[10][1]);
+					} else if (index == 1) {
+						printHundred(number);
+					} else if (index == 2) {
+						printTen(number, index);
+					} else {
+						if (number != '0' && tempN[index - 1] != '1') {
+							printOne(number);
+						}
+					}
+					break;
+				case 5:
+					if (index == 0) {
+						printTen(number, index);
+					} else if (index == 1) {
+						if (number != '0' && tempN[index - 1] != '1') {
+							printOne(number);
+						}
+						response.push(numbersMap[10][1]);
+					} else if (index == 2) {
+						printHundred(number);
+					} else if (index == 3) {
+						printTen(number, index);
+					} else {
+						if (number != '0' && tempN[index - 1] != '1') {
+							printOne(number);
+						}
+					}
+					break;
+				default:
+					response.push('');
+			}
+		});
+
+		return response.join(' ');
+	} catch (error) {
+		return '';
+	}
 };
 
-numberToEnglish(0);
+for (let i = 0; i < 10000; i++) {
+	numberToEnglish(12412);
+}
